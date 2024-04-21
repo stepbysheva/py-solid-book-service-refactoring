bca27b8f496a5d15cce3b9dcdd92166803287183
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-04-21 21:59:01 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,31 +1,24 @@
+from __future__ import annotations
+
 import json
 import xml.etree.ElementTree as ET
+from abc import abstractmethod, ABC
 
 
-class Book:
+class Book(ABC):
     def __init__(self, title: str, content: str):
         self.title = title
         self.content = content
 
-    def display(self, display_type: str) -> None:
-        if display_type == "console":
-            print(self.content)
-        elif display_type == "reverse":
-            print(self.content[::-1])
-        else:
-            raise ValueError(f"Unknown display type: {display_type}")
+    def display(self, *args, **kwargs):
+        pass
 
-    def print_book(self, print_type: str) -> None:
-        if print_type == "console":
-            print(f"Printing the book: {self.title}...")
-            print(self.content)
-        elif print_type == "reverse":
-            print(f"Printing the book in reverse: {self.title}...")
-            print(self.content[::-1])
-        else:
-            raise ValueError(f"Unknown print type: {print_type}")
 
-    def serialize(self, serialize_type: str) -> str:
+class Serializer(Book):
+    def __init__(self, title: str, content: str):
+        super().__init__(title, content)
+
+    def display(self, serialize_type):
         if serialize_type == "json":
             return json.dumps({"title": self.title, "content": self.content})
         elif serialize_type == "xml":
@@ -39,14 +32,45 @@ class Book:
             raise ValueError(f"Unknown serialize type: {serialize_type}")
 
 
+class Print(Book):
+    def __init__(self, title: str, content: str):
+        super().__init__(title, content)
+
+    def display(self, print_type: str) -> None:
+        if print_type == "console":
+            print(f"Printing the book: {self.title}...")
+            print(self.content)
+        elif print_type == "reverse":
+            print(f"Printing the book in reverse: {self.title}...")
+            print(self.content[::-1])
+        else:
+            raise ValueError(f"Unknown print type: {print_type}")
+
+
+class Display(Book):
+    def __init__(self, title: str, content: str):
+        super().__init__(title, content)
+
+    def display(self, display_type: str) -> None:
+        if display_type == "console":
+            print(self.content)
+        elif display_type == "reverse":
+            print(self.content[::-1])
+        else:
+            raise ValueError(f"Unknown display type: {display_type}")
+
+
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
         if cmd == "display":
+            book = Display(book.title, book.content)
             book.display(method_type)
         elif cmd == "print":
-            book.print_book(method_type)
+            book = Print(book.title, book.content)
+            book.display(method_type)
         elif cmd == "serialize":
-            return book.serialize(method_type)
+            book = Serializer(book.title, book.content)
+            return book.display(method_type)
 
 
 if __name__ == "__main__":
